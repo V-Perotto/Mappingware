3f9f4a48a45f7ecf9838d8f1c0ff7e654a08c3c4
V-Perotto <vittorio.perotto@outlook.com>
2023-04-28 16:17:34 -0300

    Simplificando o arquivo main.py

--- main.py
+++ main.py
@@ -1,34 +1,7 @@
-from Loggers.KeyLogger import KeyLogger
-from Loggers.ClickLogger import ClickLogger
-from Loggers.Logger import Logger
-import ctypes, sys
-import traceback
+from Files.Main import Main
 
-
-class Main(Logger):
-    
-    def run(self):
-        Logger.log_info(self, message="teste")
-
-    def is_admin(self):
-        try:
-            return ctypes.windll.shell32.IsUserAnAdmin()
-        except:
-            return False
+main = Main()
 
 if __name__ == "__main__":
-    main = Main()
-    keylog = KeyLogger()
-    clicklog = ClickLogger()
-    logger = Logger()
-    
-    if main.is_admin():
-        try:
-            keylog.keyboard_listener()
-            # clicklog.mouse_listener()
-        except:
-            logger.log_error(traceback.format_exc()) 
-    else:
-        # Re-run the program with admin rights
-        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    main.run()
         
