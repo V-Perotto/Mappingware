b3c960bac530f0252ee1ce9196e54e76723cd8ba
V-Perotto <vittorio.perotto@outlook.com>
2023-08-02 16:44:52 -0300

    Refatorando código; adicionando Docstrings

--- app/Loggers/Logger.py
+++ app/Loggers/Logger.py
@@ -4,12 +4,17 @@ from datetime import datetime
 import logging
 from Settings.config import OUTPUT_LOG
 
-output_log_format = f"%(asctime)s - [%(levelname)s] - %(message)s"
-
 class Logger():
     
-    def __init__(self, output) -> None:
-        self.output = os.path.join(OUTPUT_LOG, output)
+    def __init__(self, output: str = None) -> None:
+        self.output = OUTPUT_LOG if output is None else os.path.join(OUTPUT_LOG, output)
+    
+    def __create_directory_if_not_exists(self, directory_path: str):
+        try:
+            if not os.path.exists(directory_path):
+                os.makedirs(directory_path)
+        except OSError as e:
+            raise f"Error: {e}"
     
     def __add_kwargs_in_log(self, kwargs, args = "") -> str:
         count = 0
@@ -21,7 +26,7 @@ class Logger():
                 args += f"{key}={value}"
         return args
         
-    def _message(self, message: str = '', traceId: Optional[str] = None, **kwargs: Any):
+    def __message(self, message: str = '', traceId: Optional[str] = None, **kwargs: Any):
         msg = message
         if traceId:
             msg = f"ID: {traceId} | {message}"
@@ -33,12 +38,10 @@ class Logger():
             msg = args if message == '' else f'{msg} | {args}'
         return msg
 
-    def _create_file_log(self, datetime_now, log_per_day: bool = False) -> str:
-        if log_per_day:
-            # cria um arquivo de log por dia
+    def __create_file_log(self, datetime_now, filelog_per_day: bool = False) -> str:
+        if filelog_per_day:
             return f"{self.output}/{datetime_now.strftime('%Y-%m-%d')}.log"
         else:
-            # cria um arquivo de log por mensagem de log
             return f"{self.output}/{datetime_now.strftime('%Y-%m-%d_%H-%M-%S-%f')}.log"
         
     def __verify_file_extension(self, filename: str) -> bool:
@@ -46,9 +49,9 @@ class Logger():
             return f"{self.output}/{filename}"                
         return f"{self.output}/{filename}.log"
            
-    def _filename(self, filename: str = None, log_per_day: bool = True) -> str:
+    def __filename(self, filename: str = None, filelog_per_day: bool = True) -> str:
         if filename == None:
-            return self._create_file_log(datetime.now(), log_per_day)
+            return self.__create_file_log(datetime.now(), filelog_per_day)
         else:
             return self.__verify_file_extension(filename)
     
@@ -56,24 +59,64 @@ class Logger():
               level, 
               message: str = '', 
               traceId: Optional[str] = None, 
-              log_format: str = output_log_format,
-              log_per_day: bool = True,  
+              log_format: str = f"%(asctime)s - [%(levelname)s] - %(message)s",
+              filelog_per_day: bool = True,  
               **kwargs: Any) -> None:
-        logging.basicConfig(filename=self._filename(log_per_day=log_per_day), level=level, format=f"{log_format}")
-        msg = self._message(message, traceId=traceId, **kwargs)
+        logging.basicConfig(
+            filename=self.__create_directory_if_not_exists(
+                self.__filename(filelog_per_day=filelog_per_day)
+                ), 
+            level=level, 
+            format=f"{log_format}")
+        msg = self.__message(message, traceId=traceId, **kwargs)
         return msg
     
     def debug(self, message: str = '', traceId: Optional[str] = None, **kwargs: Any):
+        """Debug Level
+
+        Args:
+            message (str, optional): -> Defaults to ''.
+            traceId (Optional[str], optional): -> Defaults to None.
+            **kwargs (Any, optional).
+        """
         logging.debug(self.__log(level=logging.DEBUG, message=message, traceId=traceId, **kwargs))
         
     def info(self, message: str = '', traceId: Optional[str] = None, **kwargs: Any):
+        """Info Level
+
+        Args:
+            message (str, optional): -> Defaults to ''.
+            traceId (Optional[str], optional): -> Defaults to None.
+            **kwargs (Any, optional).
+        """
         logging.info(self.__log(level=logging.INFO, message=message, traceId=traceId, **kwargs))
         
     def warn(self, message: str = '', traceId: Optional[str] = None, **kwargs: Any):
+        """Warning Level
+
+        Args:
+            message (str, optional): -> Defaults to ''.
+            traceId (Optional[str], optional): -> Defaults to None.
+            **kwargs (Any, optional).
+        """
         logging.warning(self.__log(level=logging.WARNING, message=message, traceId=traceId, **kwargs))
         
     def error(self, message: str = '', traceId: Optional[str] = None, **kwargs: Any):
+        """Error Level
+
+        Args:
+            message (str, optional): -> Defaults to ''.
+            traceId (Optional[str], optional): -> Defaults to None.
+            **kwargs (Any, optional).
+        """
         logging.error(self.__log(level=logging.ERROR, message=message, traceId=traceId, **kwargs))
         
     def critical(self, message: str = '', traceId: Optional[str] = None, **kwargs: Any):
+        """Critical Level
+
+        Args:
+            message (str, optional): -> Defaults to ''.
+            traceId (Optional[str], optional): -> Defaults to None.
+            **kwargs (Any, optional).
+        """
         logging.critical(self.__log(level=logging.CRITICAL, message=message, traceId=traceId, **kwargs))
