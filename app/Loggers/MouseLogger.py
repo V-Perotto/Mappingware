6d4a3b19497fa929183623faa38ddd7e363b4004
V-Perotto <vittorio.perotto@outlook.com>
2023-08-02 16:45:16 -0300

    Refatorando código; removendo código sem uso

--- app/Loggers/MouseLogger.py
+++ app/Loggers/MouseLogger.py
@@ -1,37 +1,26 @@
-import time
 from pynput.mouse import Listener
 from Loggers.Logger import Logger
+import time
 
 logger = Logger("Mouse")
 
 class MouseLogger():
     
     def __init__(self) -> None:
-        self.clicks = []
+        self.clicks = []        
     
-    def set_clicks(self, click) -> None:
-        self.clicks.append(click)
-        
-    def get_clicks(self) -> list:
-        return self.clicks
-        
-    def on_click(self, button_name) -> None:
-        timestamp = time.time()
-        self.clicks.append((timestamp, button_name))
-        
-    def get_clicks(self) -> list:
-        return self.clicks
-    
-    def on_move(self, x, y) -> None:
+    def __on_move(self, x, y) -> None:
         logger.info(message="Mouse moved to ({0}, {1})".format(x, y))
 
-    def on_click(self, x, y, button, pressed) -> None:
+    def __on_click(self, x, y, button, pressed) -> None:
+        # timestamp = time.time()
+        # self.clicks.append((timestamp, button))
         if pressed:
             logger.info(message='Mouse clicked at ({0}, {1}) with {2}'.format(x, y, button))
 
-    def on_scroll(self, x, y, dx, dy) -> None:
+    def __on_scroll(self, x, y, dx, dy) -> None:
         logger.info(message='Mouse scrolled at ({0}, {1}) ({2}, {3})'.format(x, y, dx, dy))
 
     def mouse_listener(self) -> None:
-        with Listener(on_move=self.on_move, on_click=self.on_click, on_scroll=self.on_scroll) as listener:
+        with Listener(on_move=self.__on_move, on_click=self.__on_click, on_scroll=self.__on_scroll) as listener:
             listener.join()
